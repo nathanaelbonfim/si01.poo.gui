04455094c014ed7693ed99bf25c797a9266849e2
Nathanael <nathanael.bonfim@edu.unipar.br>
2021-09-07 11:59:23 -0300

    aula05: Adição da tela cidades

--- src/telas/TelaCadastroCidade.java
+++ src/telas/TelaCadastroCidade.java
@@ -1,7 +1,23 @@
 package telas;
 
+import javax.swing.JTextField;
+
 public class TelaCadastroCidade extends TelaCadastro {
+    private JTextField jtfNome   = new JTextField(20);
+    private JTextField jtfCodigo = new JTextField(10);
+    private JTextField jtfEstado  = new JTextField(20);
+
     public TelaCadastroCidade() {
         super("Cadastro de Cidade");
+        montaTela();
+        pack();
+    }
+
+    private void montaTela() {
+        adicionaComponente(3, 1, 1, 2, "Nome", jtfNome);
+        adicionaComponente(1, 1, 1, 1, "Código", jtfCodigo);
+        adicionaComponente(5, 1, 1, 1, "Estado", jtfEstado);
+
+        habilitaComponentes(false);
     }
 }
